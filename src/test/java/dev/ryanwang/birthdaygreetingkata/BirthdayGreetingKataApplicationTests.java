0419f4403502000e07fdeb72003663b801749ce6
r41318 <r41318@gmail.com>
2022-07-15 01:23:30 +0800

    project init

---
+++ src/test/java/dev/ryanwang/birthdaygreetingkata/BirthdayGreetingKataApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ryanwang.birthdaygreetingkata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirthdayGreetingKataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
