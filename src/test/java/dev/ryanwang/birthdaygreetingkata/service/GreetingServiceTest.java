75730ea4775595b6bb962802dd56f762b2b5d49b
ryan.wang <ryan.wang@aotter.net>
2022-07-17 21:26:48 +0800

    test: add BeforeAll for set up mock today

--- src/test/java/dev/ryanwang/birthdaygreetingkata/service/GreetingServiceTest.java
+++ src/test/java/dev/ryanwang/birthdaygreetingkata/service/GreetingServiceTest.java
@@ -3,7 +3,9 @@ package dev.ryanwang.birthdaygreetingkata.service;
 import dev.ryanwang.birthdaygreetingkata.dto.GreetingDTO;
 import dev.ryanwang.birthdaygreetingkata.po.UserPO;
 import dev.ryanwang.birthdaygreetingkata.repository.UserRepository;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,7 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest
 class GreetingServiceTest {
 
@@ -24,13 +26,16 @@ class GreetingServiceTest {
     @MockBean
     private UserRepository userRepository;
 
-
-    @Test
-    void findGreetingList() {
+    @BeforeAll
+    void setUp(){
         // mock today
         LocalDate today = LocalDate.of(2022, 8, 8);
         Mockito.mockStatic(LocalDate.class).when(LocalDate::now).thenReturn(today);
+    }
 
+
+    @Test
+    void findGreetingList() {
         // mock userRepository
         final List<UserPO> userPOList = new ArrayList<>();
         userPOList.add(new UserPO("uuid-1", "Robert", "Yen", "Male", LocalDate.parse("1985-08-08"), "robert.yen@linecorp.com"));
@@ -43,13 +48,8 @@ class GreetingServiceTest {
 
     @Test
     void findGreetingListNoResult() {
-        // mock today
-        LocalDate today = LocalDate.of(2022, 7, 8);
-        Mockito.mockStatic(LocalDate.class).when(LocalDate::now).thenReturn(today);
-
         // mock userRepository
         final List<UserPO> userPOList = new ArrayList<>();
-        userPOList.add(new UserPO("uuid-1", "Robert", "Yen", "Male", LocalDate.parse("1985-08-08"), "robert.yen@linecorp.com"));
         Mockito.when(userRepository.findByBirthSpecific(8, 8)).thenReturn(userPOList);
 
         List<GreetingDTO> greetingDTOList = greetingService.findGreetingList();
