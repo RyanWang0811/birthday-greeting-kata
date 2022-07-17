cc2da93713c74c0f86c8126d9d1797e40be0a1bd
ryan.wang <ryan.wang@aotter.net>
2022-07-17 17:10:10 +0800

    feature: add UserPO constructor

--- src/main/java/dev/ryanwang/birthdaygreetingkata/po/UserPO.java
+++ src/main/java/dev/ryanwang/birthdaygreetingkata/po/UserPO.java
@@ -34,6 +34,15 @@ public class UserPO {
     @Column(name = "email")
     private String email;
 
+    public UserPO(String uuid, String firstName, String lastName, String gender, LocalDate birth, String email) {
+        this.uuid = uuid;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.birth = birth;
+        this.email = email;
+    }
+
     public String getUuid() {
         return uuid;
     }
